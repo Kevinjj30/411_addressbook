cce3a3922cf5f3f4eb017984d5cf086a66f4131c
kevinjj30 <kevinjenkins56@yahoo.com>
2020-01-14 06:16:51 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) { 
+    super(props);
+    this.state = { 
+      isLoading: true, 
+      results: [] 
+      
+    }
+  }
+
+  componentDidMount() { 
+    this.fetchData(); 
+  }
+
+  fetchData() {
+    fetch ('https://randomuser.me/api?results=25') 
+    .then(response => response.json()) 
+    .then(parsedJSON => parsedJSON.results.map(results=> ({ 
+      name: `${results.name.first} ${results.name.last}`,
+      picture: `${results.picture.thumbnail}`
+     })))
+    .then(results=> this.setState({
+      results,
+      isLoading: false 
+    }))
+    .catch(error => console.log("parsing failed", error)) 
 }
+
+render() {
+  const {isLoading, results} = this.state;
+  return (
+    <div className="body">
+      <header>Address List</header>
+    
+     <div className={`content ${isLoading ? 'is-loading' : ''}`}> {}
+        <div className="results-info">
+          {
+           
+            !isLoading && results.length > 0 ? results.map(result => { 
+              const {name, picture} = result; 
+              return <div key={results} title={name}> {}
+                <p className="name-line">{name} <button className="like-button">Details</button></p>
+                <img src={picture}/>
+            
+                <br></br>
+                </div>
+            }) : null 
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+}
+
+
+
+
+
+
+
 
 export default App;
