daf68eeee4271acfe213300b2a974c47027e77dd
Kevinjj30 <kevinjenkins56@yahoo.com>
2020-01-14 11:39:21 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Toggle from './toggle';
 
 class App extends React.Component {
   constructor(props) { 
@@ -20,9 +21,17 @@ class App extends React.Component {
     fetch ('https://randomuser.me/api?results=25') 
     .then(response => response.json()) 
     .then(parsedJSON => parsedJSON.results.map(results=> ({ 
-      name: `${results.name.first} ${results.name.last}`,
-      picture: `${results.picture.thumbnail}`
-     })))
+      Name: `${results.name.first} ${results.name.last}`,
+      Picture: `${results.picture.thumbnail}`,
+      CityandState: `${results.location.city} ${results.location.state}`,
+      Address: `${results.location.street.number} ${results.location.street.name}`,
+      Email: `${results.email}`,
+      Username: `${results.login.username}`,
+      Password: `${results.login.password}`,
+      DOB: `${results.dob.date}`,
+      Age: `${results.dob.age}`,
+      Cell: `${results.cell}`
+})))
     .then(results=> this.setState({
       results,
       isLoading: false 
@@ -34,19 +43,23 @@ render() {
   const {isLoading, results} = this.state;
   return (
     <div className="body">
-      <header>Address List</header>
+      <header>Contact List</header>
     
      <div className={`content ${isLoading ? 'is-loading' : ''}`}> {}
         <div className="results-info">
           {
            
             !isLoading && results.length > 0 ? results.map(result => { 
-              const {name, picture} = result; 
-              return <div key={results} title={name}> {}
-                <p className="name-line">{name} <button className="like-button">Details</button></p>
-                <img src={picture}/>
-            
-                <br></br>
+              const {Name, Picture, Email} = result; 
+              return <div key={results} title={Name}> {}
+                <li className="Name">{Name}</li>
+               <img src={Picture}/>
+               <Toggle>
+               <li className="Email">{Email}</li>
+
+               </Toggle>
+               
+               <br></br>
                 </div>
             }) : null 
           }
